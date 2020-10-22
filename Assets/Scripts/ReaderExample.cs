1bac94e3b34f4205d40f5ebe736830cc94c51f17
Claudio <claudio.torresaviles@gmail.com>
2020-10-22 10:21:53 -0300

    Update read JSON file method

--- Assets/Scripts/ReaderExample.cs
+++ Assets/Scripts/ReaderExample.cs
@@ -7,80 +7,96 @@ using UnityEngine.UI;
 
 public class ReaderExample : MonoBehaviour
 {
-    public int countRow = 1;
+    string actualData = "";
+    //Text prefab for cells in JSON data table
     public Text uIText;
+    //Title text for dynamically change the JSON data table's title
     public Text uiTitle;
+    //Parent transform for headers UI components
     public Transform headersParents;
+    //Parent transform prefab for rows UI components
     public GameObject Row;
+    //Parent transform for content on scroll view
     public Transform ContentTable;
     void Start()
     {
         ReadJsonData();
     }
+    //Code for reading data on JSON file
     public void ReadJsonData()
     {
         try{
+            //read JSON file
             using(StreamReader stream = new StreamReader(Application.streamingAssetsPath + "/JsonChallenge.json")){
                 string jsonStringData = stream.ReadToEnd();
+                if (jsonStringData != actualData){
+                    actualData = jsonStringData;
+                }else{
+                    return;
+                }
                 TitleHeaders _titleHeadersData = JsonConvert.DeserializeObject<TitleHeaders>(jsonStringData);
                 uiTitle.text = _titleHeadersData.Title;
-                countRow = 1;
-                bool setHeaders = false;
+                
+                //Content Table must be active
                 ContentTable.gameObject.SetActive(true);
+                int countHeaders = 0;
+                //Read headers
+                foreach(string headerName in _titleHeadersData.ColumnHeaders){
+                    Text uiTextHeader = null;
+                    //If there are not enough headers ui element to fill ColumnHeaders quantity, then it will create them.
+                    if (countHeaders >= headersParents.childCount){
+                        uiTextHeader = Instantiate(uIText, new Vector3(0, 0, 0), Quaternion.identity) as Text; 
+                        uiTextHeader.fontStyle = FontStyle.Bold;
+                        uiTextHeader.transform.SetParent(headersParents);
+                    }else{
+                        //Search in order if there are enough headers so it can be changed if JSON file changed.
+                        uiTextHeader = headersParents.GetChild(countHeaders).GetComponent<Text>();
+                    }
+                    uiTextHeader.text = headerName;
+                    uiTextHeader.gameObject.SetActive(true);
+                    countHeaders++;
+                }
+                if(headersParents.childCount > _titleHeadersData.ColumnHeaders.Length) {
+                    for(int i = _titleHeadersData.ColumnHeaders.Length; i< headersParents.childCount; i++){
+                        headersParents.GetChild(i).gameObject.SetActive(false);
+                    }
+                }
+                int countRow = 0;
                 foreach(Dictionary<string, string> dataInCh in _titleHeadersData.Data){
                     GameObject uiRow = null;
-                    if ((ContentTable.childCount - 1) < _titleHeadersData.Data.Count){
+                    if (countRow >= (ContentTable.childCount - 1)){
                         uiRow = Instantiate(Row, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
                         uiRow.transform.SetParent(ContentTable);
                         uiRow.name = uiRow.name+'_'+countRow;
                     }else{
-                        uiRow = ContentTable.GetChild(countRow).gameObject;
+                        uiRow = ContentTable.GetChild(countRow+1).gameObject;
                         uiRow.gameObject.SetActive(true);
                     }
-                    int countHeaders = 0;
+                    int countElementsRow = 0;
                     foreach(string headerName in _titleHeadersData.ColumnHeaders){
                         Text uiTextRow = null;
-                        if (!setHeaders){
-                            Text uiTextHeader = null;
-                            if (headersParents.childCount < _titleHeadersData.ColumnHeaders.Length){
-                                uiTextHeader = Instantiate(uIText, new Vector3(0, 0, 0), Quaternion.identity) as Text; 
-                                uiTextHeader.fontStyle = FontStyle.Bold;
-                                uiTextHeader.transform.SetParent(headersParents);
-                            }else{
-                                uiTextHeader = headersParents.GetChild(countHeaders).GetComponent<Text>();
-                            }
-                            uiTextHeader.text = headerName;
-                            uiTextHeader.gameObject.SetActive(true);
-                        }
-                        if ((uiRow.transform.childCount) < _titleHeadersData.ColumnHeaders.Length){
+                        if (countElementsRow >= uiRow.transform.childCount){
                             uiTextRow = Instantiate(uIText, new Vector3(0, 0, 0), Quaternion.identity) as Text;
                         }else{
-                            uiTextRow = uiRow.transform.GetChild(countHeaders).GetComponent<Text>();
-                            Debug.Log(countHeaders);
+                            uiTextRow = uiRow.transform.GetChild(countElementsRow).GetComponent<Text>();
+                            Debug.Log(countElementsRow);
                         }
-                        if (dataInCh[headerName] == null){
+                        if (!dataInCh.ContainsKey(headerName)){
                             uiTextRow.text = "";
                         }else{
                             uiTextRow.text = dataInCh[headerName];
                         }
                         uiTextRow.transform.SetParent(uiRow.transform);
-                        uiTextRow.gameObject.SetActive(true); 
-                        countHeaders++;
-                        if(headersParents.childCount > _titleHeadersData.ColumnHeaders.Length) {
-                            for(int i = _titleHeadersData.ColumnHeaders.Length; i< headersParents.childCount; i++){
-                                headersParents.GetChild(i).gameObject.SetActive(false);
-                            }
-                        }
-                        if(uiRow.transform.childCount > _titleHeadersData.ColumnHeaders.Length) {
-                            for(int i = _titleHeadersData.ColumnHeaders.Length; i< headersParents.childCount; i++){
-                                uiRow.transform.GetChild(i).gameObject.SetActive(false);
-                            }
+                        uiTextRow.gameObject.SetActive(true);
+                        countElementsRow++;
+                    }
+                    if(uiRow.transform.childCount > _titleHeadersData.ColumnHeaders.Length) {
+                        for(int i = _titleHeadersData.ColumnHeaders.Length; i < uiRow.transform.childCount; i++){
+                            uiRow.transform.GetChild(i).gameObject.SetActive(false);
                         }
                     }
-                    setHeaders = true;
                     countRow++;
                 }
-                
                 if((ContentTable.childCount - 1) > _titleHeadersData.Data.Count){
                     for(int i = _titleHeadersData.Data.Count+1; i< ContentTable.childCount; i++){
                         ContentTable.GetChild(i).gameObject.SetActive(false);
@@ -90,7 +106,7 @@ public class ReaderExample : MonoBehaviour
         }catch(Exception ex){
             Debug.Log(ex);
             ContentTable.gameObject.SetActive(false);
-            uiTitle.text = "Ocurrio un error en la lectura de datos. Este error puede deberse a que falte alguna llave en alguna de las filas.";
+            uiTitle.text = "An error ocurred. It may be due to some discrepancies between ColumnHeaders' quantity/names and Data array elements' parameters quantity/names.";
         }
     }
 }
